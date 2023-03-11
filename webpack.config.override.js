d196df75beb121093736d771a1f0e12fa09c5bd2
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-11 16:02:13 +0700

    change webpack

--- webpack.config.override.js
+++ webpack.config.override.js
@@ -5,3 +5,18 @@ module.exports = function override(config, env) {
   }
   return config;
 };
+
+module.exports = function override(config, env) {
+  if (env === "development") {
+    config.devServer.proxy = {
+      "/api": {
+        target: "https://api-englishapp-production.up.railway.app",
+        changeOrigin: true,
+      },
+    };
+    config.devServer.headers = { "Access-Control-Allow-Origin": "*" };
+    config.devServer.disableHostCheck = true;
+    ;
+  }
+  return config;
+};
