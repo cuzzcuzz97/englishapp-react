34d61e36225bd8f7ffb48f2f86b3d7f61599c52d
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-11 14:36:56 +0700

    bugssssss

--- webpack.config.override.js
+++ webpack.config.override.js
@@ -1,5 +1,6 @@
 module.exports = function override(config, env) {
   if (env === "development") {
+    config.devServer.headers = { "Access-Control-Allow-Origin": "*" };
     config.devServer.disableHostCheck = true;
   }
   return config;
